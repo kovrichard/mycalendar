a429a39c2534803cbe9bf4100569cf1a65c34c48
Richard Kovacs <richardkovacs.it@gmail.com>
2020-10-03 00:14:27 +0200

    Add table

--- mycalendar/main/api.py
+++ mycalendar/main/api.py
@@ -1,8 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, current_app, render_template, request
+from flask.views import MethodView
 
 main_bp = Blueprint("main", __name__, template_folder="templates")
 
 
-@main_bp.route("/", methods=["GET"])
-def welcome():
-    return render_template("welcome.html")
+class CalendarController(MethodView):
+    def get(self):
+        current_app.logger.info("GET")
+
+        return render_template("welcome.html")
+
+    def post(self):
+        current_app.logger.info("heh")
+
+        return render_template("welcome.html")
+
+
+main_bp.add_url_rule("/", view_func=CalendarController.as_view("calendar"))
