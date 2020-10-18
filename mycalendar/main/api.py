e5d38e9086e1370dfe6edd62394b82e6d027e403
Richard Kovacs <richardkovacs.it@gmail.com>
2020-10-18 13:08:48 +0200

    Redirect main to week view

--- mycalendar/main/api.py
+++ mycalendar/main/api.py
@@ -1,17 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app, request, redirect, url_for
 from flask.views import MethodView
 from flask_user import login_required
+from datetime import datetime, date
 
 main_bp = Blueprint("main", __name__, template_folder="templates")
 
-
-class CalendarController(MethodView):
-    @login_required
-    def get(self):
-        return render_template("welcome.html")
-
-    def post(self):
-        return render_template("welcome.html")
-
-
-main_bp.add_url_rule("/", view_func=CalendarController.as_view("calendar"))
+@main_bp.route("/", methods=["GET"])
+@login_required
+def get_main():
+    return redirect(url_for("week.get_week", week_num=datetime.now().isocalendar()[1]), 302)
