4e1393281d70501cf087018af6e3b20696440a55
Richard Kovacs <richardkovacs.it@gmail.com>
2020-10-18 14:21:39 +0200

    Remove auth from main

--- mycalendar/main/api.py
+++ mycalendar/main/api.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 
 from flask import Blueprint, redirect, url_for
-from flask_user import login_required
 
 main_bp = Blueprint("main", __name__, template_folder="templates")
 
 
 @main_bp.route("/", methods=["GET"])
-@login_required
 def get_main():
     return redirect(
         url_for("week.get_week", week_num=datetime.now().isocalendar()[1]), 302
--- tests/main/api_test.py
+++ tests/main/api_test.py
@@ -1,20 +1,11 @@
 from truth.truth import AssertThat
 
-from tests import (
-    AppTestCase,
-    DbMixin,
-    TemplateRenderMixin,
-    TestClientMixin,
-    logged_in_user,
-)
+from tests import AppTestCase, TemplateRenderMixin, TestClientMixin
 
 
-class ApiTest(TestClientMixin, DbMixin, TemplateRenderMixin, AppTestCase):
-    @logged_in_user
+class MainTest(TestClientMixin, TemplateRenderMixin, AppTestCase):
     def test_get_main_redirects_to_week_view(self):
-        # with self.logged_in_user():
         r = self.client.get("/")
-        template, context = self.rendered_templates[0]
+
         AssertThat(r.status_code).IsEqualTo(302)
         AssertThat(r.headers["Location"]).Contains("/week/")
-        AssertThat(1).IsEqualTo(2)
