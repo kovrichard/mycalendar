8b2df46b4a6d3580fe6ee7be1935006a7eece3ff
Richard Kovacs <richardkovacs.it@gmail.com>
2020-10-18 18:15:54 +0200

    Fix login redirect

--- mycalendar/server/config.py
+++ mycalendar/server/config.py
@@ -4,4 +4,4 @@ DB_URL = os.environ.get("DB_URL")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 USER_ENABLE_EMAIL = False
-USER_AFTER_LOGIN_ENDPOINT = "week.get_week"
+USER_AFTER_LOGIN_ENDPOINT = "main.get_main"
