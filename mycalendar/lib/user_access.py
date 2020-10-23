d22664706dfff77cd1913a9670181bc5c1c3f252
Richard Kovacs <richardkovacs.it@gmail.com>
2020-10-23 16:57:59 +0200

    Add token generation

---
+++ mycalendar/lib/user_access.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+
+import jwt
+
+class UserAccess:
+    def __init__(self, secret):
+        self.secret = secret
+
+    def generate(self, user_id, validity_period: timedelta):
+        return jwt.encode(
+            {
+                "user_id": user_id,
+                "iat": datetime.utcnow(),
+                "exp": datetime.utcnow() + validity_period,
+            },
+            self.secret,
+            algorithm="HS256",
+        ).decode()
+    
+    def decode(self, token):
+        try:
+            decoded_token = jwt.decode(
+            token,
+            self.secret,
+            algorithm="HS256",
+            options={"require_iat": True, "require_exp": True},
+        )
+        except jwt.DecodeError:
+            return None
+        except jwt.ExpiredSignatureError:
+            return None
+        
+        return decoded_token["user_id"]
---
+++ tests/lib/user_access_test.py
@@ -0,0 +1,36 @@
+from truth.truth import AssertThat
+
+from mycalendar.lib.user_access import UserAccess
+
+from datetime import timedelta
+
+import unittest
+
+USER_ID = 3
+
+class UserAccessTest(unittest.TestCase):
+    def setUp(self):
+        self.user_access = UserAccess("SECRET")
+        self.validity_period = timedelta(days=1)
+        self.token = self.user_access.generate(USER_ID, self.validity_period)
+
+    def test_generate_generates_user_access_token(self):
+        AssertThat(self.token).IsNotNone()
+
+    def test_decode_decodes_user_access_token(self):
+        user_id = self.user_access.decode(self.token)
+        AssertThat(user_id).IsEqualTo(USER_ID)
+
+    def test_decode_denies_access_for_invalid_secret(self):
+        token = UserAccess("DIFFERENT_SECRET").generate(USER_ID, self.validity_period)
+
+        AssertThat(self.user_access.decode(token)).IsNone()
+
+    def test_decode_expiration_date_is_taken_into_account(self):
+        token = self.user_access.generate(USER_ID, timedelta(days=-1))
+
+        AssertThat(self.user_access.decode(token)).IsNone()
+
+    def test_generate_raises_error_if_no_expiration_is_set(self):
+        with self.assertRaises(TypeError):
+            self.user_access.generate(USER_ID)
