4197ea75bedf38d3f3cd4cde1e68030e1c03b8de
Richard Kovacs <richardkovacs.it@gmail.com>
2020-10-25 15:34:51 +0100

    Move days of week to own function

---
+++ mycalendar/lib/calculate_week.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+def calculate_days_of_week(year, week):
+    day_of_week = []
+    for i in range(1, 8):
+        day_of_week.append(
+            {
+                "date": datetime.fromisocalendar(year, week, i).date(),
+                "name": datetime.fromisocalendar(year, week, i)
+                .date()
+                .strftime("%A"),
+            }
+        )
+    
+    return day_of_week
--- mycalendar/main/api.py
+++ mycalendar/main/api.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from flask import Blueprint, redirect, url_for
+from mycalendar.lib.calculate_week import calculate_days_of_week
 
 main_bp = Blueprint("main", __name__, template_folder="templates")
 
@@ -9,4 +10,6 @@ main_bp = Blueprint("main", __name__, template_folder="templates")
 def get_main():
     now = datetime.now().isocalendar()
 
-    return redirect(url_for("week.get_week", year=now[0], week=now[1]), 302)
+    days_of_week = calculate_days_of_week(now[0], now[1])
+
+    return redirect(url_for("week.get_week", year=now[0], week=now[1], days_of_week=days_of_week), 302)
