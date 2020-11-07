7db55415ba119db9bc9e37e8efe1ba1407757a91
Richard Kovacs <richardkovacs.it@gmail.com>
2020-11-07 19:07:25 +0100

    Make main test better

--- tests/main/api_test.py
+++ tests/main/api_test.py
@@ -2,10 +2,14 @@ from truth.truth import AssertThat
 
 from tests import AppTestCase, TemplateRenderMixin, TestClientMixin
 
+from datetime import datetime
+
 
 class MainTest(TestClientMixin, TemplateRenderMixin, AppTestCase):
     def test_get_main_redirects_to_week_view(self):
         r = self.client.get("/")
 
+        now = datetime.now().isocalendar()
+
         AssertThat(r.status_code).IsEqualTo(302)
-        AssertThat(r.headers["Location"]).Contains("/")
+        AssertThat(r.headers["Location"]).Contains(f"{self.app.config['CALENDAR_URL']}/{now[0]}/{now[1]}")
