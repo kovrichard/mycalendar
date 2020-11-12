ea57dcd799ec3e9bd890699938e1189c274242b5
Kovi10 <kovagomate.email@gmail.com>
2020-11-12 21:49:24 +0100

    Flash only related messages

--- mycalendar/week/api.py
+++ mycalendar/week/api.py
@@ -169,7 +169,7 @@ def __check_overlapping_events(new_event):
     ).all()
 
     if len(wrong_events) > 0:
-        flash("Overlapping Event")
+        flash("Overlapping Event", "danger")
         return True
 
     return False
