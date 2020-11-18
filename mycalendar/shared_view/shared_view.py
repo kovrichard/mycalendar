73f9bdf7a9fbdd2ba33969209ff9cc1fc676731a
Kovi10 <kovagomate.email@gmail.com>
2020-11-18 20:33:19 +0100

    Create Shared week view class

--- mycalendar/shared_view/shared_view.py
+++ mycalendar/shared_view/shared_view.py
@@ -97,7 +97,7 @@ def register_guest(token):
     now = datetime.now().isocalendar()
     return redirect(
         url_for(
-            "shared_view.shared_calendar",
+            "shared_view.week",
             year=now[0],
             week=now[1],
             token=token,
@@ -106,78 +106,79 @@ def register_guest(token):
     )
 
 
-@shared_view_bp.route(
-    "/<int:year>/<int:week>/shared-calendar/<string:token>",
-    methods=["GET"],
-)
-def shared_calendar(year, week, token):
-    decoded_token = UserAccess(
-        current_app.config["SHARING_TOKEN_SECRET"]
-    ).decode(token)
+class SharedWeekView(MethodView):
+    def get(self, year, week, token):
+        decoded_token = UserAccess(
+            current_app.config["SHARING_TOKEN_SECRET"]
+        ).decode(token)
 
-    if not decoded_token:
-        abort(401)
+        if not decoded_token:
+            abort(401)
 
-    year, week = date_time_helper.calculate_different_year(year, week)
+        year, week = date_time_helper.calculate_different_year(year, week)
 
-    current_week = __persist_week_to_db(year, week)
-    days_of_week = date_time_helper.calculate_days_of_week(year, week)
+        current_week = self.__persist_week_to_db(year, week)
+        days_of_week = date_time_helper.calculate_days_of_week(year, week)
 
-    events = Event.query.filter_by(
-        week_id=current_week.id, user_id=decoded_token["user_id"]
-    ).all()
+        events = Event.query.filter_by(
+            week_id=current_week.id, user_id=decoded_token["user_id"]
+        ).all()
 
-    return render_template(
-        "shared-week.html",
-        year_number=year,
-        week_number=week,
-        days_of_week=days_of_week,
-        events=__format_for_render(events),
-        shared_calendar=True,
-        share_content=decoded_token["share_content"],
-        shared_user_name=User.query.filter_by(id=decoded_token["user_id"])
-        .first()
-        .username,
-        token=token,
-    )
+        return render_template(
+            "shared-week.html",
+            year_number=year,
+            week_number=week,
+            days_of_week=days_of_week,
+            events=self.__format_for_render(events),
+            shared_calendar=True,
+            share_content=decoded_token["share_content"],
+            shared_user_name=User.query.filter_by(id=decoded_token["user_id"])
+            .first()
+            .username,
+            token=token,
+        )
 
+    def __persist_week_to_db(self, year, week):
+        tmp = Week.query.filter_by(year=year, week_num=week).first()
 
-def __persist_week_to_db(year, week):
-    tmp = Week.query.filter_by(year=year, week_num=week).first()
+        if tmp is None:
+            tmp = Week(year=year, week_num=week)
+            db.session.add(tmp)
+            db.session.commit()
 
-    if tmp is None:
-        tmp = Week(year=year, week_num=week)
-        db.session.add(tmp)
-        db.session.commit()
+        return tmp
 
-    return tmp
+    def __format_for_render(self, events):
+        tmp = []
 
+        for e in events:
+            item = {}
+            item["title"] = e.title
+            item["location"] = e.location
+            item["day"] = e.start.date().isocalendar()[2] - 1
+            item["hour"] = [
+                h
+                for h in range(
+                    e.start.time().hour,
+                    24 if e.end.time().hour == 0 else e.end.time().hour,
+                )
+            ]
+            item["type"] = "active-event"
 
-def __format_for_render(events):
-    tmp = []
+            tmp.append(item)
 
-    for e in events:
-        item = {}
-        item["title"] = e.title
-        item["location"] = e.location
-        item["day"] = e.start.date().isocalendar()[2] - 1
-        item["hour"] = [
-            h
-            for h in range(
-                e.start.time().hour,
-                24 if e.end.time().hour == 0 else e.end.time().hour,
-            )
-        ]
-        item["type"] = "active-event"
-
-        tmp.append(item)
-
-    return tmp
+        return tmp
 
 
 shared_view_bp.add_url_rule(
     "/shared-event/<string:token>",
     strict_slashes=False,
-    view_func=SharedEventView.as_view("shared_view"),
+    view_func=SharedEventView.as_view("event"),
     methods=["POST"],
+)
+
+shared_view_bp.add_url_rule(
+    "/<int:year>/<int:week>/shared-calendar/<string:token>",
+    view_func=SharedWeekView.as_view("week"),
+    methods=["GET"],
 )
